d812b1e6e67faccaeae4333da2cf89761fcac72f
Udaya Chandana <freelancing.samchan@gmail.com>
2019-12-11 22:50:31 +0530

    Add popup window for get note name

--- NoteBook/app/src/main/java/com/samchan/notebook/MainActivity.java
+++ NoteBook/app/src/main/java/com/samchan/notebook/MainActivity.java
@@ -1,5 +1,7 @@
 package com.samchan.notebook;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,11 +10,19 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "NoteBook";
+    Context mCtx;
+    SharedPreferences mSharePref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +31,32 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mCtx = this;
+
+        mSharePref = mCtx.getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+                LayoutInflater inflater = (LayoutInflater)
+                        getSystemService(LAYOUT_INFLATER_SERVICE);
+                try {
+                    View popupView = inflater.inflate(R.layout.note_name, null);
+
+                    PopupWindow popupWindow = new PopupWindow(popupView,
+                            ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    popupWindow.setOutsideTouchable(true);
+                    popupWindow.setFocusable(true);
+                    popupWindow.setElevation(200);
+                    popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+                }
+                catch (Exception e){
+                    Log.e(TAG, e.getMessage()!=null?e.getMessage():e.toString());
+                }
             }
         });
     }
